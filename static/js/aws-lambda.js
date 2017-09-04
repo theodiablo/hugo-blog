9c2ee15f5c558a8bef39a281884bb68bcd2da068
Theodiablo <theodiablo@hotmail.com>
2017-09-04 13:32:47 +0530

    Add registration form to the sidebar

---
+++ static/js/aws-lambda.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+	var subscribeEmail = function(email, languageCode){
+		AWS.config.region = 'us-east-1'; // Region
+		AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:0a0d2bec-2e8f-4281-8abe-adfd65bb773a',});
+		var lambda = new AWS.Lambda({region: 'us-east-1', apiVersion: '2015-03-31'});
+
+		lambda.invoke({
+			FunctionName: 'subscribe-email-user',
+			Payload: JSON.stringify({
+				email: email,
+				languageCode: languageCode,
+				conversionUrl: window.location.href
+			})
+		}, function(err, data) {
+		  if (err && console) {
+			console.log(err);
+		  }
+		});
+	} 
+	
+	document.querySelector(".subscribe form").addEventListener("submit", function (event) {
+		event.preventDefault();
+		console.log("Try to subscribeEmail");
+		
+		var email = event.target.getElementsByClassName("email")[0].value;
+
+		if(email){
+			subscribeEmail(email, global.languageCode);
+		}
+	});
+}
