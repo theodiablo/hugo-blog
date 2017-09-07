94387310c5a1bb71d490b8d01de994451a642add
Theodiablo <theodiablo@hotmail.com>
2017-09-07 17:15:46 +0530

    Improve subscribe form: add success message & rename js file.

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -6,7 +6,7 @@ window.onload = function () {
 	AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:0a0d2bec-2e8f-4281-8abe-adfd65bb773a',});
 	var lambda = new AWS.Lambda({region: 'us-east-1', apiVersion: '2015-03-31'});
 
-	var subscribeEmail = function(email, name, languageCode){
+	var subscribeEmail = function(email, name, languageCode, event){
 		lambda.invoke({
 			FunctionName: 'subscribe-email-user',
 			Payload: JSON.stringify({
@@ -20,22 +20,39 @@ window.onload = function () {
 				console.log(err);
 			}
 			else{
-				createCookie(SUBSCRIBED_COOKIE_NAME, SUBSCRIBED_COOKIE_VALUE, 3650);	
+				createCookie(SUBSCRIBED_COOKIE_NAME, SUBSCRIBED_COOKIE_VALUE, 3650);
+				changeLoadingVisibility(event.target, false);
+				changeSuccessVisibility(event.target, true);
 			}
 		});
 	}
 	
 	document.querySelector(".subscribe form").addEventListener("submit", function (event) {
 		event.preventDefault();
+		changeSubmitButtonEnableState(event.target, false);
+		changeLoadingVisibility(event.target, true);
+
 		console.log("Try to subscribeEmail");
 		
 		var email = event.target.getElementsByClassName("email")[0].value;
 		var name = event.target.getElementsByClassName("name")[0].value;
 
 		if(email && name){
-			subscribeEmail(email, name, global.languageCode);
+			subscribeEmail(email, name, global.languageCode, event);
 		}
 	});
+}
+
+function changeSubmitButtonEnableState(eventTarget, isEnabled){
+	eventTarget.getElementsByClassName("submit")[0].disabled = !isEnabled;
+}
+
+function changeLoadingVisibility(eventTarget, isVisible){
+	eventTarget.parentElement.getElementsByClassName("spinner")[0].hidden = !isVisible;
+}
+
+function changeSuccessVisibility(eventTarget, isVisible){
+	eventTarget.parentElement.getElementsByClassName("success")[0].hidden = !isVisible;
 }
 
 function createCookie(name,value,days) {
