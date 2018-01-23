01139d2f5f0ed595418452eacb197b1768d43671
theodiablo <theodiablo@hotmail.com>
2018-01-23 22:21:04 +0630

    Improve windows script

--- build.cmd
+++ build.cmd
@@ -1,21 +1,17 @@
 set HUGO_ENV=PROD
 
 call gulp images
-IF %ERRORLEVEL% NEQ 0 goto error
+IF %ERRORLEVEL% NEQ 0 pause
 
 call hugo
-IF %ERRORLEVEL% NEQ 0 goto error
+IF %ERRORLEVEL% NEQ 0 pause
 
 call gulp build
-IF %ERRORLEVEL% NEQ 0 goto error
+IF %ERRORLEVEL% NEQ 0 pause
 
-aws s3 sync ./public/ s3://theodiablo-travel-blog/ --delete --acl public-read --size-only --cache-control max-age=86400 --content-encoding gzip
-IF %ERRORLEVEL% NEQ 0 goto error
+aws s3 sync ./public/ s3://theodiablo-travel-blog/ --delete --acl public-read --size-only --cache-control max-age=86400
+IF %ERRORLEVEL% NEQ 0 pause
 
 echo Build and publish Success!
 
-pause
-
-:error
-echo Something Bad Happened.
 pause
