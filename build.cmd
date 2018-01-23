abe377cc82be7f73639b0b118fe4f11975ba7392
theodiablo <theodiablo@hotmail.com>
2018-01-23 21:23:13 +0630

    Add cache control and encoding gzip when uploading to S3

--- build.cmd
+++ build.cmd
@@ -9,7 +9,7 @@ IF %ERRORLEVEL% NEQ 0 goto error
 call gulp build
 IF %ERRORLEVEL% NEQ 0 goto error
 
-aws s3 sync ./public/ s3://theodiablo-travel-blog/ --delete --acl public-read --size-only --only-show-errors
+aws s3 sync ./public/ s3://theodiablo-travel-blog/ --delete --acl public-read --size-only --cache-control max-age=86400 --content-encoding gzip
 IF %ERRORLEVEL% NEQ 0 goto error
 
 echo Build and publish Success!
