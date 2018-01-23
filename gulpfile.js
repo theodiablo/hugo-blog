64ae17653282db3789bdce67daca474c3f6fa56e
theodiablo <theodiablo@hotmail.com>
2018-01-23 12:13:13 +0630

    Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var $ = require('gulp-load-plugins')();
 var responsive = require('gulp-responsive');
+var runSequence = require('run-sequence');
 
-gulp.task('minify', minifyHtml);
-gulp.task('images', minifyHtml);
+gulp.task('build', gulp.series(minifyHtml, responsiveImg));
+
+gulp.task('html', minifyHtml);
+gulp.task('images', responsiveImg);
 
 function minifyHtml() {
   return gulp.src('public/**/*.html')
@@ -12,41 +15,67 @@ function minifyHtml() {
     .pipe(gulp.dest('public'));
 }
 
-/*
-<img
-  sizes="(min-width: 40em) 80vw, 100vw"
-  srcset="examples/images/image-medium.jpg 375w,
-          examples/images/image-large.jpg 480w,
-          examples/images/image-extralarge.jpg 768w"
-  alt="…">
-*/
 
 function responsiveImg() {
-  return gulp.src('public/images/*.{jpg,png}')
+  return gulp.src(['public/images/posts/**/*.jpg'])
     .pipe($.responsive({
       // Convert all images to JPEG format
-      '*': [{
-        // image-medium.jpg is 375 pixels wide
-        width: 375,
+      '**/*': [{
+        // image-medium.jpg is 400 pixels wide
+        width: 500,
+        withoutEnlargement: false,
         rename: {
           suffix: '-medium',
-          extname: '.jpg',
+          extname: '.jpg'
         },
       }, {
-        // image-large.jpg is 480 pixels wide
-        width: 480,
+        // image-large.jpg is 800 pixels wide
+        width: 800,
+        withoutEnlargement: false,
         rename: {
           suffix: '-large',
-          extname: '.jpg',
+          extname: '.jpg'
         },
       }, {
-        // image-extralarge.jpg is 768 pixels wide
-        width: 768,
+        // image-extralarge.jpg is 1400 pixels wide
+        width: 1400,
+        withoutEnlargement: false,
         rename: {
           suffix: '-extralarge',
-          extname: '.jpg',
+          extname: '.jpg'
         },
-      }],
+      }, 
+      //2x for retina displays
+      {
+        // image-medium.jpg is 400 pixels wide
+        width: 500 * 2,
+        withoutEnlargement: false,
+        rename: {
+          suffix: '-medium@2x',
+          extname: '.jpg'
+        },
+      }, {
+        // image-large.jpg is 800 pixels wide
+        width: 800 * 2,
+        withoutEnlargement: false,
+        rename: {
+          suffix: '-large@2x',
+          extname: '.jpg'
+        },
+      }, {
+        // image-extralarge.jpg is 1400 pixels wide
+        width: 1400 * 2,
+        withoutEnlargement: false,
+        rename: {
+          suffix: '-extralarge@2x',
+          extname: '.jpg'
+        },
+      }]
+    },
+      {
+	      // global quality for all images
+	      quality: 75,
+	      errorOnUnusedImage: false
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('public/images/generated/posts'));
 };
